48ea667404d5db462e97bac1859b24fade729e97
Natalie <natalie@sca.design>
2020-12-16 18:01:26 +0000

    Add css and animation for landing page

--- src/Components/Landing/Landing.js
+++ src/Components/Landing/Landing.js
@@ -1,40 +1,41 @@
 import './Landing.css';
+import 'animate.css';
 
 function Landing() {
     return (
-        <div className="page-container">
+        <div className="landing-container">
             <div>
                 <h1 className="font-xl">One step at a time towards a better world</h1>
             </div>
             <div className="actions-container actions-container-sm">
                 <div className="action-container action-container-sm animate__animated animate__fadeInRight animate__slow">
-                    <div className="action red min-width-110">Estimate</div>
-                    <div>Your carbon footprint</div>
+                    <div className="font-lg action red min-width-110">Estimate</div>
+                    <div className="font-md">Your carbon footprint</div>
                 </div>
                 <div
                     className="action-container action-container-sm animate__animated animate__fadeInRight animate__slow animate__delay-1s">
-                    <div className="action orange min-width-110">Track</div>
-                    <div>Your ongoing carbon usage</div>
+                    <div className="font-lg action orange min-width-110">Track</div>
+                    <div className="font-md">Your ongoing carbon usage</div>
                 </div>
                 <div
                     className="action-container action-container-sm animate__animated animate__fadeInRight animate__slow animate__delay-2s">
-                    <div className="action purple min-width-110">Join</div>
-                    <div>Your organisation or friends</div>
+                    <div className="font-lg action purple min-width-110">Join</div>
+                    <div className="font-md">Your organisation or friends</div>
                 </div>
                 <div
                     className="action-container action-container-sm animate__animated animate__fadeInRight animate__slow animate__delay-3s">
-                    <div className="action green min-width-110">Reduce</div>
-                    <div>Your impact on the planet</div>
+                    <div className="font-lg action green min-width-110 font-md">Reduce</div>
+                    <div className="font-md"> Your impact on the planet</div>
                 </div>
             </div>
-            <p>Our tracker allows you to measure, understand and track your carbon footprint as an
+            <p class="font-sm">Our tracker allows you to measure, understand and track your carbon footprint as an
             individual or an organisation. Join together with friends or your team at work to reduce your
             impact on the planet.
             </p>
             <div className="carbon-button">
-                <button type="button" className="btn btn-success">
-                    <div className="font-md">Estimate Your Carbon Footprint</div>
-                    <div className="font-sm">Start tracking today</div>
+                <button type="button">
+                    <div className="font-md padding-sm">Estimate Your Carbon Footprint</div>
+                    <div className="font-sm padding-sm">Start tracking today</div>
                 </button>
             </div>
         </div>
