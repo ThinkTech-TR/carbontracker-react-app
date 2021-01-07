e5a09b25f1b43482c77c4bf414e1fda8b238d5b8
Jakub Szymczak <jakubator@gmail.com>
2021-01-07 00:34:02 +0000

    Adding Signup button to Results page. Also changing visualization for the table with the results

--- src/Components/Results/Results.js
+++ src/Components/Results/Results.js
@@ -5,6 +5,9 @@ import "bootstrap/dist/js/bootstrap.js";
 import { Row } from "react-bootstrap";
 import { Col } from "react-bootstrap";
 import { Container } from "react-bootstrap";
+import {
+    Link
+} from "react-router-dom";
 
 
 class ApexChart extends React.Component {
@@ -13,7 +16,7 @@ class ApexChart extends React.Component {
 
         this.state = {
 
-            series: [70, 50, 44, 33, 22, 10],
+            series: [85, 50, 60, 50, 65, 15],
             options: {
                 labels: ['Flights', 'Transport', 'Diet', 'Housing', 'Products', 'Services'],
                 chart: {
@@ -31,10 +34,10 @@ class ApexChart extends React.Component {
                             },
                             total: {
                                 show: true,
-                                label: 'Total',
+                                label: 'TOTAL',
                                 formatter: function (w) {
                                     // By default this function returns the average of all series. The below is just an example to show the use of custom formatter function
-                                    return 229
+                                    return 10704 + ' Kg'
                                 }
                             }
                         }
@@ -59,17 +62,74 @@ class ApexChart extends React.Component {
                         </div>
                     </Col>
                     <Col xs={12} md={6}>
-                        <b><p>Your carbon footprint can be much lower. Try to reduce your flights to help out the planet</p></b>
-                        <ul className="list-group" style={{columns: 2}}>
-                            <li className="list-group-item">Flights <span className="badge">70</span></li>
-                            <li className="list-group-item">Transport <span className="badge">50</span></li>
-                            <li className="list-group-item">Diet <span className="badge">44</span></li>
-                            <li className="list-group-item">Housing <span className="badge">33</span></li>
-                            <li className="list-group-item">Products <span className="badge">22</span></li>
-                            <li className="list-group-item">Services <span className="badge">10</span></li>
-                        </ul>
+                        <div className="font-sm"><p>Your carbon footprint could be much lower. Try to reduce number of your flights or choose low emission flights to help out the planet</p></div>
+                        <table className="table table-hover">
+                            <thead>
+                                <tr>
+                                    <th scope="col">#</th>
+                                    <th scope="col">Type</th>
+                                    <th scope="col">Carbon Footpint in Kg</th>
+                                    <th scope="col">Average in Kg</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr className="bg-danger">
+                                    <th scope="row">1</th>
+                                    <td>Flights</td>
+                                    <td>7000</td>
+                                    <td>4500</td>
+                                </tr>
+                                <tr class="table-warning">
+                                    <th scope="row">2</th>
+                                    <td>Transport</td>
+                                    <td>500</td>
+                                    <td>650</td>
+                                </tr>
+                                <tr class="table-warning">
+                                    <th scope="row">3</th>
+                                    <td>Diet</td>
+                                    <td>1500</td>
+                                    <td>2200</td>
+                                </tr>
+                                <tr class="table-success">
+                                    <th scope="row">4</th>
+                                    <td>Housing</td>
+                                    <td>900</td>
+                                    <td>1642</td>
+                                </tr>
+                                <tr class="table-success">
+                                    <th scope="row">5</th>
+                                    <td>Products</td>
+                                    <td>654</td>
+                                    <td>1200</td>
+                                </tr>
+                                <tr class="table-info">
+                                    <th scope="row">6</th>
+                                    <td>Services</td>
+                                    <td>150</td>
+                                    <td>200</td>
+                                </tr>
+                            </tbody>
+                        </table>
                     </Col>
                 </Row>
+                <Row className="row">
+                    <div className="font-sm">
+                        <strong>Sign up </strong>today to save your results.<br />
+                        <ul>
+                            <li><strong>Track</strong> Your ongoing carbon usage.</li>
+                            <li><strong>Compete</strong> with Your organisation colleagues or friends.</li>
+                            <li><strong>Reduce</strong> Your impact on the planet Today.</li>
+                        </ul>
+                        
+                        
+                        
+                    <div style={{ marginTop: "5px" }}>
+                            <Link to="/login"><button type="button" className="btn btn-success d-md-block">Sign up now!</button></Link>
+                        </div>
+                    </div>
+                </Row>
+
             </Container>
         );
     }
