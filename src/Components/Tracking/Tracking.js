fa2afd52e3363d19cea34f4de1603f7c4cabd85b
Zap-Master <kim.lidiya2011@gmail.com>
2021-01-13 23:52:02 +0000

    fix responsive for table, add a drop-down for diet

--- src/Components/Tracking/Tracking.js
+++ src/Components/Tracking/Tracking.js
@@ -4,7 +4,8 @@ import Image from 'react-bootstrap/Image';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import Table from 'react-bootstrap/Table'
+import Table from 'react-bootstrap/Table';
+//import Dropdown from 'react-bootstrap/DropdownButton';
 import './Tracking.css';
 
 
@@ -30,37 +31,66 @@ function Tracking () {
                                 </thead>
                                 <tbody>
                                     <tr>
+                                        
                                         <td>Housing</td>
                                         <td>10 kg</td>
+                                        <td></td>
                                         <td></td>
                                     </tr>
                                     <tr>
                                         <td>Car</td>
                                         <td>8 kg</td>
                                         <td>15 mi</td>
+                                        <td></td>
                                     </tr>
                                     <tr>
-                                        <td>Diet pescatarian</td>
+                                        <td>
+                                            <select className="diet-select" id="diet">
+                                                <option value="Pescatarian">Pescatarian</option>
+                                                <option value="Meat at most">Meat at most meal</option>
+                                                <option value="Occasionally meat">Occasionally meat</option>
+                                            </select>
+                                        </td>
                                         <td>0.5 kg</td>
+                                        <td>
+                                        </td>
                                         <td></td>
                                     </tr>
                                     <tr>
                                         <td>Bus</td>
                                         <td>2 kg</td>
                                         <td>15 mi</td>
+                                        <td></td>
                                     </tr>
                                     <tr>
                                         <td>Train</td>
                                         <td>2 kg</td>
                                         <td>15 mi</td>
+                                        <td></td>
                                     </tr>
                                 </tbody>
                             </Table>
                         </div>
 
-                        <div className="button-day-container">
-                            <button className="button-green">Back</button> 
-                        </div>
+
+                        
+                            <Table className="button-day-container">
+                                <tbody>
+                                    <tr>
+                                        <td className="tdButtonBack">
+                                            <button className="button-green">Back</button>
+                                        </td>
+                                        <td className="tdButtonForward">
+                                            <button className="button-green">Forward</button>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            
+                            
+
+                            </Table>
+
+                        
                     </div>
 
                 </Col>
