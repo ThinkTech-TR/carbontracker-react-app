b31396f50a721cb13b2cf1aefce09ea052fb4b5b
jakubator <jakubator@gmail.com>
2021-01-18 22:12:19 +0000

    Analyze component init

---
+++ src/Components/Analyze/Analyze.js
@@ -0,0 +1,24 @@
+import './Analyze.css';
+
+import {
+    Link
+} from "react-router-dom";
+
+function Analyze() {
+    return (
+        <div className="analyze-container">
+            <div className="profile-container">
+                
+            </div> 
+            <div className="graphs-container">
+
+            </div>
+            <div className="leaders-container">
+
+            </div>
+        </div>
+        );
+    }
+    
+    export default Analyze;
+    
