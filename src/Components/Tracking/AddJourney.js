6dae4ca64a59ed1ccd18afecee0ebe5f50f215e5
Zap-Master <kim.lidiya2011@gmail.com>
2021-01-20 00:05:32 +0000

    add checks (Mileage and Journey are not empty) before adding a new item for tracking

--- src/Components/Tracking/AddJourney.js
+++ src/Components/Tracking/AddJourney.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { useState } from "react";
 import journeyIco from "./journey-icon.jpg";
 import Image from 'react-bootstrap/Image';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlusSquare} from "@fortawesome/free-regular-svg-icons";
 import "./AddJourney.css";
 
+
+
 function AddJourneyForm({addJourney}) {
+    
+    const selectJourney = useRef(null);
+    const inputMileage = useRef(null);
+
     const [trackingItemId, setTrackingItemId] = useState("");
     const [trackingItemName, setTrackingItemName] = useState("");
     const [distance, setDistance] = useState("");
@@ -17,41 +25,58 @@ function AddJourneyForm({addJourney}) {
             break;
         default:
             setTrackingItemId(event.target.value);
+            console.log(event.target.value);
             setTrackingItemName(event.target.options[event.target.selectedIndex].text);
         }
     }
 
-    
     const handleClick = () => {
-        addJourney(trackingItemId, trackingItemName, distance);
-        setDistance("");
-        setTrackingItemId("");
-        setTrackingItemName("");
+        if (trackingItemId === "") {
+            alert("Please choose a journey");
+            selectJourney.current.focus();
+        }else if (distance === "") {
+            alert("Please add a mileage")
+            inputMileage.current.focus();
+        }
+        else {
+            addJourney(trackingItemId, trackingItemName, distance);
+            setDistance("");
+            setTrackingItemId("");
+            setTrackingItemName("");
+        }
+
     }
     
 return (
     <div className="form-inline">
-        <Image src={journeyIco} alt="total" />
-        <p className="font-sm">Journey by </p>
-        <select
-            id="journeyType" 
-            className="font-sm"
-            onChange={ handleChange }>
-                <option value="2">Car</option>
-                <option value="4">Bus</option>
-                <option value="5">Train</option>
-                <option value="6">Plane</option>
-        </select>
-        <p className="font-sm">distance </p>
-        <input 
-            onChange={ handleChange }
-            id="distance" 
-            type="number" 
-            min="0" 
-            className="font-sm" 
-            placeholder="miles">
-        </input>
-        <button onClick={handleClick} type="submit" className="button-green">+</button>
+            {/*<Image src={journeyIco} alt="total" />*/}
+            {/*<label htmlfor="journeyType" className="font-sm">Journey by </label>*/}
+            <select 
+                ref = { selectJourney }
+                value = { trackingItemId }
+                id="journeyType" 
+                className="font-sm"
+                onChange={ handleChange }>
+                    <option value="0">Please choose a journey</option>
+                    <option value="2">Car</option>
+                    <option value="4">Bus</option>
+                    <option value="5">Train</option>
+                    <option value="6">Plane</option>
+            </select>
+            {/*<p className="font-sm">distance </p>*/}
+            <input 
+                ref = { inputMileage }
+                onChange={ handleChange }
+                id="distance" 
+                type="number" 
+                min="0" 
+                className="font-sm" 
+                placeholder="miles"
+                value={ distance }>
+            </input>
+            <button onClick={handleClick} type="submit" className="button-green"><FontAwesomeIcon icon={ faPlusSquare } className="icon-size" /></button>
+        
+        
     </div>
     );
 }
